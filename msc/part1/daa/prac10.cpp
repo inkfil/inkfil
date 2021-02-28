0ed46ab809d454f44fc923811249700a8d896352
ronaktheshinigami <ronakjainrjrjrj@gmail.com>
2021-02-28 18:45:00 +0530

    Update prac10.cpp

--- msc/part1/daa/prac10.cpp
+++ msc/part1/daa/prac10.cpp
@@ -1,3 +1,31 @@
 // Prac 10
 // Aim:
 #include<iostream>
+using namespace std;
+int findMinX(int num[], int rem[], int k){
+	int x = 1;
+	while(true){
+		int j;
+		for(j=0; j<k; j++){
+			if(x%num[j] != rem[j]){
+				break;
+			}
+		}
+
+		// If all remainders matched, we found x
+		if(j == k){
+			return x;
+		}
+		x++;
+	}
+	return x;
+}
+
+int main(void){
+	int num[] = {3, 4, 5};
+	int rem[] = {2, 3, 1};
+	int k = sizeof(num)/sizeof(num[0]);
+	std::cout << "x is " << findMinX(num, rem, k);
+	return 0;
+}
+
