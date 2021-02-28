0b24d301a77d8eb751120647d2408aeae138588b
ronaktheshinigami <ronakjainrjrjrj@gmail.com>
2021-02-28 18:45:32 +0530

    Update extra1.cpp

--- msc/part1/daa/extra1.cpp
+++ msc/part1/daa/extra1.cpp
@@ -1,60 +1,85 @@
 // Extra 1
 // Aim:
 #include<iostream>
+#include<mutex>
 #include<thread>
 #include<chrono>
+// REFERENCE: https://stackoverflow.com/questions/30243779/c-matrix-multiplication-with-multithreads-and-semaphore
+int const size = 3;
+std::mutex someMutex[size][size];
+std::thread someThread[size];
 
-void fibonacci(long num){
-    long a=0;
-    long b=1;
-    long c=0;
-    long n=0;
-    while(n++<num){
-        std::cout<<n<<"th Fib no: = "<<a;
-        //<<std::endl;
-        c=a+b;
-        a=b;
-        b=c;
-    }    
-}
-
-void prime(long num){
-    long c=0;
-    long j=0;
-    long i=0;
-    for(i=0;i<=num;i++){
-        for(j=2;j<=i;j++){
-            if(i%j==0){
-                break;
-            }
-        }
-        if(j==i){ 
-            c++;    
-            std::cout<<c<<"th Prime no: = "<<i;
-            //<<std::endl;
-        }    
-    }    
+void MatrixMultiplication(int const * const * matrixA, int const * const * matrixB,int** matrixC, int row, int col, int i){
+	someMutex[row][col].lock();
+	matrixC[row][col] += matrixA[row][i]*matrixB[i][col];
+	someMutex[row][col].unlock();
 }
 
 int main(){
-    long num=25;
-    std::cout<<"This is main thread :)"<<std::endl;
 
-    std::cout<<"without threads: "<<std::endl;
-    auto starttime_wot = std::chrono::high_resolution_clock::now();
-    fibonacci(num);
-    prime(num);
-    auto endtime_wot = std::chrono::high_resolution_clock::now();
-    auto diff_wot = std::chrono::duration_cast<std::chrono::microseconds> (endtime_wot-starttime_wot);
+	// int size =3;
+	int** matrixA;
+	int** matrixB;
+	int** matrixC;
 
-    auto starttime_wt = std::chrono::high_resolution_clock::now();
-    std::cout<<"\nwith threads: "<<std::endl;
-    std::thread t_fib(fibonacci, num);
-    std::thread t_prime(prime, num);
-    t_fib.join();
-    t_prime.join();
-    auto endtime_wt = std::chrono::high_resolution_clock::now();
-    auto diff_wt = std::chrono::duration_cast<std::chrono::microseconds> (endtime_wt-starttime_wt);
-    std::cout<<"\nTime taken without threads: "<<diff_wot.count()<<" microseconds."<<std::endl;
-    std::cout<<"Time taken with threads: "<<diff_wt.count()<<" microseconds."<<std::endl;
+	matrixA = new int*[size];
+	matrixB = new int*[size];
+	matrixC = new int*[size];
+
+	for(int i = 0; i<size; i++){
+		matrixA[i] = new int[size];
+		matrixB[i] = new int[size];
+		matrixC[i] = new int[size];
+	}
+
+	int count = 1;
+	for(int i = 0; i<size; i++){
+		for(int j = 0; j<size; j++){
+			matrixA[i][j] = count;
+			matrixB[i][j] = count*3;
+			count++;
+		}
+	}
+
+	auto starttime = std::chrono::high_resolution_clock::now();
+	for(int row=0; row<size; row++){
+		for(int col=0; col<size;col++){
+			for(int i=0;i<size;i++){
+				someThread[i] = std::thread(MatrixMultiplication, matrixA, matrixB, matrixC, row, col, i);
+				someThread[i].join();
+			}
+		}
+	}
+	auto endtime = std::chrono::high_resolution_clock::now();
+
+	std::cout<<"This is Matrix A:"<<std::endl;
+	for(int i = 0; i<size; i++){
+		for(int j = 0; j<size; j++){
+			std::cout<<matrixA[i][j]<<"\t";
+		}
+		std::cout<<std::endl;
+	}
+	std::cout<<std::endl;
+
+	std::cout<<"This is Matrix B:"<<std::endl;
+	for(int i = 0; i<size; i++){
+		for(int j = 0; j<size; j++){
+			std::cout<<matrixB[i][j]<<"\t";
+		}
+		std::cout<<std::endl;
+	}
+	std::cout<<std::endl;
+
+	std::cout<<"This is Matrix C (product of matrix A and matrix B):"<<std::endl;
+	for(int i = 0; i<size; i++){
+		for(int j = 0; j<size; j++){
+			std::cout<<matrixC[i][j]<<"\t";
+		}
+		std::cout<<std::endl;
+	}
+	std::cout<<std::endl;
+	auto diff = std::chrono::duration_cast<std::chrono::microseconds> (endtime - starttime);
+	std::cout<<"Time to multiply matrix : "<<diff.count()<<" microseconds";
+
+	return 0;
 }
